ced5aaeebd5226012575a55386c5f182bce4c6df
CrhistoferVera <cristofervera110@gmail.com>
2026-06-06 10:59:46 -0400

    Add Oktava logo and update Logo component to use Next.js Image

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface LogoProps {
@@ -8,17 +9,19 @@ interface LogoProps {
 export function Logo({ mobileSize = 140, desktopSize = 264 }: LogoProps) {
   return (
     <Link href="/" className="flex justify-center">
-      <img
-        src="https://res.cloudinary.com/dai7rtja6/image/upload/v1774284573/Logo_Base_SVG_1_elipfb.svg"
+      <Image
+        src="/oktava_logo.png"
         alt="OKTAVA"
-        className="object-contain rounded-full border-3 border-white mt-5 block md:hidden"
-        style={{ width: mobileSize, height: mobileSize }}
+        width={mobileSize}
+        height={Math.round(mobileSize / 4)}
+        className="object-contain mt-5 block md:hidden"
       />
-      <img
-        src="https://res.cloudinary.com/dai7rtja6/image/upload/v1774284573/Logo_Base_SVG_1_elipfb.svg"
+      <Image
+        src="/oktava_logo.png"
         alt="OKTAVA"
-        className="object-contain rounded-full border-3 border-white mt-5 hidden md:block"
-        style={{ width: desktopSize, height: desktopSize }}
+        width={desktopSize}
+        height={Math.round(desktopSize / 4)}
+        className="object-contain mt-5 hidden md:block"
       />
     </Link>
   );
